1e4bd775e9d6c52c41d42b98cabbc17b1dc6abeb
Aayush09123 <110332733+Aayush09123@users.noreply.github.com>
2024-03-14 18:30:31 +0530

    make login component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
+import UserContextProvider from './context/UserContextProvider';
 
 function App() {
 
   return (
-    <>
+    <UserContextProvider>
       <h1>React Context API</h1>
-    </>
+    </UserContextProvider>
   )
 }
 
